19ce74564b455438b2152ebaeaf8b2cb9b49d6ef
SALAH-NAME <>
2025-02-12 21:16:42 +0100

    add: uncomment for ray-castring only

--- src/core/ray_intersection_shading.c
+++ src/core/ray_intersection_shading.c
@@ -18,5 +18,7 @@ t_color	ray_intersection_shading(t_ray ray, t_scene *scene)
 
 	if (find_nearest_intersection(ray, scene, &hit))
 		return (calculate_lighting(&hit, scene));
+	//	RAY-CASTING
+	//	return (hit.material.color);
 	return ((t_color){BACKGROUND_COLOR});
 }
